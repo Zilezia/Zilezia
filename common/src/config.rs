2f95458e93d37df25df3c6f49b1d84a54f04dc7d
Zilezia <czeriwanczak@gmail.com>
2024-09-17 11:12:16 +0100

    cloning for rpi

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,15 +1,15 @@
 use dotenv::from_filename;
-use std::env;
+use std::env::var;
 
 pub fn load_env() { // change to dotenv.ok?
     from_filename("./common/.env").ok();
 }
 pub fn get_mysql_url() -> String {
-    let user = env::var("DB_USER").expect("DB_USER must be set.");
-    let password = env::var("DB_PASSWORD").expect("DB_PASSWORD must be set.");
-    let host = env::var("DB_HOST").expect("DB_HOST must be set.");
-    let port = env::var("DB_PORT").expect("DB_PORT must be set.");
-    let db_name = env::var("DB_NAME").expect("DB must be set.");
+    let user = var("DB_USER").expect("DB_USER must be set.");
+    let password = var("DB_PASSWORD").expect("DB_PASSWORD must be set.");
+    let host = var("DB_HOST").expect("DB_HOST must be set.");
+    let port = var("DB_PORT").expect("DB_PORT must be set.");
+    let db_name = var("DB_NAME").expect("DB must be set.");
     
     format!(
         "mysql://{}:{}@{}:{}/{}", 
@@ -17,16 +17,24 @@ pub fn get_mysql_url() -> String {
     )
 }
 
+// pub fn get_table() -> String {
+//     var("TABLE").expect("BASE_URL must be set.")
+// }
+
 pub fn get_table() -> String {
-    env::var("TABLE").expect("TABLE must be set.")
+    match var("TABLE") {
+        Ok(table) => table,
+        Err(_) => "default_table".to_string(),
+    }
 }
+
 pub fn get_base_url() -> String {
-    env::var("BASE_URL").expect("BASE_URL must be set.")
+    var("BASE_URL").expect("BASE_URL must be set.")
 }
 pub fn get_ip() -> String {
-    env::var("IPH").expect("IPH must be set.")
+    var("IPH").expect("IPH must be set.")
 }
 // this one needs to be an uint, a bit
 pub fn get_port() -> String {
-    env::var("PORT").expect("PORT must be set.")
+    var("PORT").expect("PORT must be set.")
 }
