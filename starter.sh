865ce3b8f790d8d450485afb0d777cb370b4b764
Zilezia <czeriwanczak@gmail.com>
2024-09-26 15:02:19 +0100

    under construction phase2

--- starter.sh
+++ starter.sh
@@ -1,5 +1,6 @@
-
-export LIP=zilezia.dev
+# idk how else to read from env in yew so just set it like this
+export 
+export HIP=zilezia.dev
 echo "Building frontend"
 cd ./front
 trunk build --release
