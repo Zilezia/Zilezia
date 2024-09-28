aa135e85d2e92c2eafbcaa697f559ed36c80cdb8
Zilezia <czeriwanczak@gmail.com>
2024-09-28 22:54:47 +0100

    needed commit

--- front/src/pages/well_be_back.rs
+++ front/src/pages/well_be_back.rs
@@ -11,7 +11,7 @@ pub fn well_be_back() -> Html {
                 {"Under construction please give us"}<code>{" (whos us? im alone)"}</code>
             </h2>
             <h2 class="announce_title">
-                {"some time to finish!"}<code>{" (maybe like 1-3 days idfk)"}</code>
+                {"some time to finish!"}<code title="~29/09/2024" >{" (maybe like 1-3 days idfk)"}</code>
             </h2>
         </div>
         <div class="img_div">
